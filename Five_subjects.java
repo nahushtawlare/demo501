96e9295865ae29b448bd95fd6ccbe35e772f2ae0
nahushtawlare <114064828+nahushtawlare@users.noreply.github.com>
2022-09-21 17:25:35 +0530

    Add files via upload

---
+++ Five_subjects.java
@@ -0,0 +1,22 @@
+package cdac_classes;
+
+public class Five_subjects {
+
+	public static void main(String[] args) {
+		// wap to print average of  given five subjects marks of student and 
+		//check if average >=40 print Pass else print fail
+       int a=25;
+       int b=25;
+       int c=25;
+       int d=25;
+       int e=25;
+       if((a+b+c+d+e)/5>=40) {
+    	   System.out.println("result is pass");
+       }
+       else
+    	   {
+    	   System.out.println("result is fail");
+    	   }
+	}
+
+}
