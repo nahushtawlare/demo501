4178494dd6883ea123d60a2de1d9a8eaf22b2274
nahushtawlare <114064828+nahushtawlare@users.noreply.github.com>
2022-09-21 17:07:24 +0530

    my pc
    program file

---
+++ Discountprice.java
@@ -0,0 +1,21 @@
+package cdac_classes;
+
+import java.util.Scanner;
+
+public class Discountprice {
+
+	public static void main(String[] args) {
+		// Twap to ask product name and price of product from user and calculate discount i.e 
+	//	if price > 2000 then discount is 10 percent of price 
+	//	else discount is 7 % of price
+	
+		Scanner s= new Scanner(System.in);
+        string productname;
+        int    productprice;
+        System.out.println("name and price of product");
+        productname=s.nextLine();
+        productprice=s.nextInt();
+
+	}
+
+}
